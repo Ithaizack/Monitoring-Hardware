72a90a0e5cb590b8a7f59423231abe6e9ed358b0
Ithaizack <isaacabreu499@gmail.com>
2022-11-21 23:54:31 -0300

    Colocando para funcionamento

--- index.js
+++ index.js
@@ -1,12 +1,36 @@
-const cpu = require('node:os')
-const array = [
-    {
-        Titulo:"Vasco",
-        Valor: 0
-    },
-    {
-        Titulo:"Galçao",
-        Valor: 12223432
+
+const os = require('node:os')
+
+const cpu = os.cpus()
+
+const {model} = cpu[1]
+
+const arch = os.arch()
+
+const network = os.networkInterfaces()["Ethernet"]
+
+const address = network.filter(scope=>{
+    return scope.family == "IPv4"
+})
+
+setInterval(()=>{
+
+    console.clear()
+    const memory = parseInt(os.totalmem() / 1024 / 1024)
+    const usage = parseInt(os.freemem() / 1024 / 1024)
+    const porcents = parseInt((usage / memory) * 100)
+
+    const MonitoringMemory = {
+        TotalMemory: memory + " MB",
+        Using: usage + " MB",
+        porcents: porcents + "%"
     }
-]
-console.table(array)
+
+    console.log("\n Processador: ",model,"\n Arquitetura: ",arch)
+    console.log("\n Memorys")
+    console.table(MonitoringMemory)
+    console.table(" Internets:")
+    console.table(address)
+
+
+},1000)
