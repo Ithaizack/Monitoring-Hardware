bb8e67d7ddcdb03d0f051e3b0947f647126b9faf
Ithaizack <isaacabreu499@gmail.com>
2022-11-21 21:37:17 -0300

    Primeiro commit do projeto

---
+++ index.js
@@ -0,0 +1,12 @@
+const cpu = require('node:os')
+const array = [
+    {
+        Titulo:"Vasco",
+        Valor: 0
+    },
+    {
+        Titulo:"Galçao",
+        Valor: 12223432
+    }
+]
+console.table(array)
